d23a7de64240af7bf80f03df0de9d1fe91547437
navanee1609 <navaneethanvs18@gmail.com>
2024-03-14 13:49:03 +0530

    created title section

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './title.css'
+function Title() {
+  return (
+    <div className='title'>
+        <p>Our Program</p>
+        <h2>What we offer</h2>
+    </div>
+  )
+}
+
+export default Title
