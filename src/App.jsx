956f273166f48f1220c27af5f370785dada29acc
navanee1609 <navaneethanvs18@gmail.com>
2024-03-14 14:03:24 +0530

    added styling to title

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Programs from './components/programs/Programs'
-
+import Title from './components/title/Title'
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <div className="container">
+        <Title/>
       <Programs/>
       </div>
     </div>
