342c840943cd8e077f9a2f51ecade548fa334bdd
navanee1609 <navaneethanvs18@gmail.com>
2024-03-14 15:55:45 +0530

    added campus images and mounted in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/hero/Hero'
 import Programs from './components/programs/Programs'
 import Title from './components/title/Title'
 import About from './components/about/About'
+import Campus from './components/campus/Campus'
 function App() {
   return (
     <div>
@@ -13,6 +14,8 @@ function App() {
         <Title subTitle='Our Program' title='What we offer'/>
       <Programs/>
       <About/>
+      <Title subTitle='Gallery' title=' Campus Images'/>
+      <Campus/>
       </div>
     </div>
   )
