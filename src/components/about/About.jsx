c80ce1de5a5c0027335f07a19bcaeb02f2052d06
navanee1609 <navaneethanvs18@gmail.com>
2024-03-14 14:31:57 +0530

    provided styling to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,15 +6,15 @@ function About() {
   return (
     <div className='about'>
         <div className="about-left">
-            <img src={about_img} alt="" />
-            <img src={play_icon} alt="" />
+            <img src={about_img} alt="" className='about-img'/>
+            <img src={play_icon} alt="" className='play-icon'/>
         </div>
         <div className="about-right">
             <h3>ABOUT UNIVERSITY</h3>
             <h2>Nurturing Tomorrow Leader today</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat at rerum omnis doloribus maxime, nulla quis nam consectetur pariatur esse? Temporibus ex, numquam ducimus id nisi, quas qui delectus eaque magnam modi iusto!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quibusdam quos neque voluptate reiciendis sequi nihil doloribus nemo dolore facere accusamus nobis illo, officiis officia alias laudantium excepturi aperiam saepe nisi ipsam quisquam vero eaque inventore veritatis. Labore quasi veniam voluptates saepe. Libero, eius.</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur neque laboriosam aliquam dolore minus tenetur suscipit et commodi eum, repudiandae officia blanditiis!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat at rerum omnis doloribus maxime? Temporibus ex, numquam ducimus id nisi, quas qui delectus eaque magnam modi iusto!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quibusdam quos neque voluptate reiciendis sequi nihil  illo, officiis officia alias laudantium excepturi aperiam saepe nisi ipsam quisquam vero eaque inventore veritatis. Labore quasi veniam voluptates saepe. Libero, eius.</p>
+            <p>Lorem ipsum dolor, sit amet officia blanditiis!</p>
         </div>
 
     </div>
