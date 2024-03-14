57da23301dce824f65058602fc456506a2b1c3e8
navanee1609 <navaneethanvs18@gmail.com>
2024-03-14 12:36:04 +0530

    added styling to hero section and completed hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,14 @@
 // import React from 'react'
 import './hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 function Hero() {
   return (
-    <div className='hero'>
-
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>We ensure better education for a better world</h1>
+            <p>Our Cutting-Edge curriculum is designed to empower students with knowledge, skills,and experiences needed to excel in the dynamic field of education</p>
+            <button className="btn">Explore more <img src={dark_arrow} alt="" /></button>
+        </div>
     </div>
   )
 }
