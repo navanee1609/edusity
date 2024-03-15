73cf0fe727c3b3f6c9702d3823ee13efa1ce99d7
navanee1609 <navaneethanvs18@gmail.com>
2024-03-15 11:30:54 +0530

    assigned styling to contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,6 +4,7 @@ import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import location_icon from '../../assets/location-icon.png'
 import phone_icon from '../../assets/phone-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 function Contact() {
   return (
     <div className="contact">
@@ -17,7 +18,21 @@ function Contact() {
             </ul>
             
         </div>
-        <div className="contact-col"></div>
+        <div className="contact-col">
+            <form>
+                <label>Your Name</label>
+                <input type="text" name='name' placeholder='Enter Your name' required />
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter Your Contact Number' required />
+                <label>E-mail</label>
+                <input type="email" name='mail' placeholder='Enter Your E-mail' required />
+                <label>Leave Your Message</label>
+                <textarea name="message" required rows="6" placeholder='Enter Message'></textarea>
+                <button type="submit" className='btn dark-btn'>Submit <img src={white_arrow} alt="" /></button>
+
+            </form>
+            <span>sending</span>
+        </div>
     </div>
   )
 }
